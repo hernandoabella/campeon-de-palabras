bfbd212d06c968033f10744ef535fc83424a119d
Hernando Abella <hernandoabella@gmail.com>
2023-05-15 01:16:04 -0500

    mejoras en la ui

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 window.onload = init;
 
-//Globals
-
 //Available levels
 const levels = {
-    easy: 5,
-    medium: 3,
-    hard: 1
-}
+  easy: 5,
+  medium: 3,
+  hard: 1,
+};
 
 //To change levels
 const currentLevel = levels.easy;
@@ -17,140 +15,103 @@ let score = 0;
 let isPlaying;
 
 //DOM elements
-const wordInput = document.querySelector('#word-input');
-const currentWord = document.querySelector('#current-word');
-const scoreDisplay = document.querySelector('#score');
-const timeDisplay = document.querySelector('#time');
-const message = document.querySelector('#message');
-const seconds = document.querySelector('#seconds');
+const wordInput = document.querySelector("#word-input");
+const currentWord = document.querySelector("#current-word");
+const scoreDisplay = document.querySelector("#score");
+const timeDisplay = document.querySelector("#time");
+const message = document.querySelector("#message");
+const seconds = document.querySelector("#seconds");
 
 //Word array
 const words = [
-    'width',
-    'unlikely',
-    'acceptable',
-    'depressed',
-    'loot',
-    'disappear',
-    'default',
-    'manufacture',
-    'pastel',
-    'vegetable',
-    'budget',
-    'opera',
-    'pool',
-    'mirror',
-    'secular',
-    'laser',
-    'censorship',
-    'interference',
-    'leash',
-    'pay',
-    'satisfied',
-    'pepper',
-    'allocation',
-    'club',
-    'patent',
-    'fuss',
-    'lake',
-    'commission',
-    'president',
-    'simplicity',
-    'technique',
-    'magnitude',
-    'favor',
-    'color-blind',
-    'paper',
-    'assignment',
-    'abnormal',
-    'cultural',
-    'transport',
-    'threaten',
-    'quaint',
-    'mug',
-    'major',
-    'compromise',
-    'favorable',
-    'appreciate',
-    'check',
-    'van',
-    'referral',
-    'bell'
+  "width",
+  "unlikely",
+  "acceptable",
 ];
 
 //Initialize Game
 function init() {
-    // Show number of seconds in UI
-    seconds.innerHTML = currentLevel;
-    // Load word from array
-    showWord(words);
-    // Start matching on word inputs
-    wordInput.addEventListener('input', startMatch);
-    // Call countdown every second
-    setInterval(countdown, 1000);
-    // Check game status
-    setInterval(checkStatus, 50);
+  // Show number of seconds in UI
+  seconds.innerHTML = currentLevel;
+  // Load word from array
+  showWord(words);
+  // Start matching on word inputs
+  wordInput.addEventListener("input", startMatch);
+  // Call countdown every second
+  setInterval(countdown, 1000);
+  // Check game status
+  setInterval(checkStatus, 50);
 }
 
 // Start Match
 function startMatch() {
-    if(matchWords()) {
-        isPlaying = true;
-        time = currentLevel + 1;
-        showWord(words);
-        wordInput.value = '';
-        score++;
-    }
+  if (matchWords()) {
+    isPlaying = true;
+    time = currentLevel + 1;
+    showWord(words);
+    wordInput.value = "";
+    score++;
+  }
 
-    // If score is -1, show 0
-    if(score == -1){
-        scoreDisplay.innerHTML = 0;
-    }
-    else {
-        scoreDisplay.innerHTML = score;
-    }
+  // If score is -1, show 0
+  if (score == -1) {
+    scoreDisplay.innerHTML = 0;
+  } else {
+    scoreDisplay.innerHTML = score;
+  }
 }
 
 // Match current word to word input
 function matchWords() {
-    if (wordInput.value == currentWord.innerHTML){
-        message.innerHTML = 'Correct!';
-        return true;
-    }
-    else{
-        message.innerHTML = '';
-        return false;
-    }
-} 
+  if (wordInput.value == currentWord.innerHTML) {
+    message.innerHTML = "Correct!";
+    return true;
+  } else {
+    message.innerHTML = "";
+    return false;
+  }
+}
 
 //Pick and show random word
 function showWord(words) {
-    //Generate random array index
-    const randIndex = Math.floor(Math.random() * words.length);
-    //Output random word
-    currentWord.innerHTML = words[randIndex];
+  //Generate random array index
+  const randIndex = Math.floor(Math.random() * words.length);
+  //Output random word
+  currentWord.innerHTML = words[randIndex];
 }
 
 //Countdown timer
 function countdown() {
-    // If the time is not run out
-    if (time > 0){
-        //Decrement
-        time--;
-    }
-    // If the time has run out
-    else if(time == 0) {
-        //Game is over
-        isPlaying = false;
-    }
-    //Show time
-    timeDisplay.innerHTML = time;
+  // If the time is not run out
+  if (time > 0) {
+    //Decrement
+    time--;
+  }
+  // If the time has run out
+  else if (time == 0) {
+    //Game is over
+    isPlaying = false;
+  }
+  //Show time
+  timeDisplay.innerHTML = time;
 }
 
 //Check game status
 function checkStatus() {
-    if(!isPlaying && time == 0){
-        message.innerHTML = 'Game Over!';
-        score = -1;
-    }
+  if (!isPlaying && time == 0) {
+    message.innerHTML = "Game Over!";
+    score = -1;
+  }
 }
+
+const dialogBox = document.getElementById("dialog-box");
+const openBtn = document.getElementById("open-btn");
+const closeBtn = document.getElementById("close-btn");
+
+openBtn.addEventListener("click", () => {
+  dialogBox.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+  dialogBox.style.display = "none";
+});
